fe09971549d24395d331d8950c8548c179dbea80
Kenny Grossman <mrkengrossman@gmail.com>
2019-04-28 23:39:34 -0700

    Added try catch to all modules and added yellow border to signify an issue.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function (){
-    //Check to see if config.json file exists
-    //If not create file and load setup
+    //TODO: Check to see if config.json file exists
 
-    //If so continue
+    //If found show content
         refreshPage();
-
         let refreshInterval = setInterval(refreshPage, 100000)
+
+    //TODO: If not found
 });
 
 function refreshPage(){
@@ -24,6 +24,7 @@ function refreshPage(){
 }
 
 function timeUpdater(){
+    try{
         //variables used for date and time calculation
         let d = new Date();
         let hours = d.getHours();
@@ -31,7 +32,7 @@ function timeUpdater(){
         // let seconds = d.getSeconds();
         let AMPM = (hours > 12) ? "PM" : "AM";
 
-        //Make formatting adjustments to minutes and hours if necessary
+        //Format time if necessary 
         hours = (hours > 12) ? (hours-= 12) : hours;
         minutes = (minutes < 10) ? ('0' + minutes) : minutes;
         // seconds = (seconds < 10) ? ('0' + seconds) : seconds;
@@ -47,21 +48,26 @@ function timeUpdater(){
         //Assign formatted string to Date and Time
         $("#date").append(dateString);
         $("#time").append(timeString);
+
+        //Set border color if successful
+        $("#events").addClass('whiteBorder');
+        $("#events").removeClass('yellowBorder');
+    }
+    catch(err){
+        //Set border color if error
+        $("#events").addClass('yellowBorder');
+        $("#events").removeClass('whiteBorder');
+    }
 }       
 
 function weatherUpdater(){
-    //Build an API URL
-    let URLbase = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-    let zipCode = config.WEATHER.ZIP_CODE;
-    let apiKey = config.WEATHER.WEATHER_KEY;
-    let units = 'imperial';
+    try{
+        //Build an API URL
+        let URLbase = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+        let zipCode = config.WEATHER.ZIP_CODE;
+        let apiKey = config.WEATHER.WEATHER_KEY;
+        let units = 'imperial';
 
-    //Check if values are missing from config.js file
-    if(zipCode == null || apiKey == null){
-        alert("Missing values from config.js... unable to load weather module");
-        // $("#weather").addClass('yellowBorder');
-    }
-    else{
         let requestURL = URLbase + zipCode + ',us&units=' + units + '&appid=' + apiKey; 
 
         //Send ajax request
@@ -91,85 +97,109 @@ function weatherUpdater(){
                 console.log("Error connecting to OpenWeatherMap API: " + apiKey);
             }
         });
+        //Set border color if successful
+        $("#weather").addClass('whiteBorder');
+        $("#weather").removeClass('yellowBorder');
+    }
+    catch(err){
+        //Set border color if error
+        $("#weather").addClass('yellowBorder');
+        $("#weather").removeClass('whiteBorder');
     }
 }
 
 function stocksUpdater(){
-    //Get stock symbols from config.json
-    let stocks = config.MONEY.STOCKS;
+    try{
+        //Get stock symbols from config.json
+        let stocks = config.MONEY.STOCKS;
 
-    //Clear Stock data before updating
-    $("#stockSymbol").empty();
-    $("#stockPrice").empty();
+        //Clear Stock data before updating
+        $("#stockSymbol").empty();
+        $("#stockPrice").empty();
 
-    //Iterate through each stock in array finding JSON then parsing data
-    $.each(stocks, function (index, value){
-        let baseURL = 'https://api.iextrading.com/1.0/stock/';
-        let endURL = '/quote'        
-        let requestURL = baseURL + value + endURL;
+        //Iterate through each stock in array finding JSON then parsing data
+        $.each(stocks, function (index, value){
+            let baseURL = 'https://api.iextrading.com/1.0/stock/';
+            let endURL = '/quote'        
+            let requestURL = baseURL + value + endURL;
 
-        //Send ajax request
-        $.ajax({
-            url: requestURL,
-            success: function(data) {
-                //Add new list items in respective <ul> elements
-                $("#stockSymbol").append('<li>'+ data.symbol + ":" +'</li>');
+            //Send ajax request
+            $.ajax({
+                url: requestURL,
+                success: function(data) {
+                    //Add new list items in respective <ul> elements
+                    $("#stockSymbol").append('<li>'+ data.symbol + ":" +'</li>');
 
-                //Decide if stock is up or down
-                let color;
-                if (data.latestPrice > data.open)
-                    color = 'green';
-                else
-                    color = 'red';
+                    //Decide if stock is up or down
+                    let color;
+                    if (data.latestPrice > data.open)
+                        color = 'green';
+                    else
+                        color = 'red';
 
-                $("#stockPrice").append('<li class = " ' + color + '">' + parseFloat(data.latestPrice).toFixed(2) + '</li>');
-            }, 
-            error: function() {
-                console.log("Error connecting to iexTrading API");
-            },
-            type: 'GET'
+                    $("#stockPrice").append('<li class = " ' + color + '">' + parseFloat(data.latestPrice).toFixed(2) + '</li>');
+                }, 
+                error: function() {
+                    console.log("Error connecting to iexTrading API");
+                },
+                type: 'GET'
+            });
         });
-    });
+        //Set border color if successful
+        $("#money").addClass('whiteBorder');
+        $("#money").removeClass('yellowBorder');
+    }
+    catch(err){
+        //Set border color if error
+        $("#money").addClass('yellowBorder');
+        $("#money").removeClass('whiteBorder');
+    }
 }
 
 function coinUpdater(){
-    //Get coin symbols from config.json
-    let coins = config.MONEY.COINS;
-    //
+    try{
+        //Get coin symbols from config.json
+        let coins = config.MONEY.COINS;
 
-    //Create array of coins that user is interested in. 
-    // let coins = ['bitcoin', 'ethereum', 'litecoin'];
+        //Clear Coin data before updating
+        $("#coinSymbol").empty();
+        $("#coinPrice").empty();
 
-    //Clear Coin data before updating
-    $("#coinSymbol").empty();
-    $("#coinPrice").empty();
+        $.each(coins, function (index, value){
+            let baseURL = 'https://api.coinmarketcap.com/v1/ticker/'
+            let requestURL = baseURL + value + '/';
 
-    $.each(coins, function (index, value){
-        let baseURL = 'https://api.coinmarketcap.com/v1/ticker/'
-        let requestURL = baseURL + value + '/';
+            //Send ajax request
+            $.ajax({
+                url: requestURL,
+                success: function(data) {
+                    //Add new list items in respective <ul> elements
+                    $("#coinSymbol").append('<li>'+ data[0].symbol + ":" +'</li>');
+                    console.log(data[0]);
+                    //Decide if coin is up or down in the past 24 hours
+                    let color;
+                    if (data[0].percent_change_24h > 0)
+                        color = 'green';
+                    else
+                        color = 'red';
 
-        //Send ajax request
-        $.ajax({
-            url: requestURL,
-            success: function(data) {
-                //Add new list items in respective <ul> elements
-                $("#coinSymbol").append('<li>'+ data[0].symbol + ":" +'</li>');
-                console.log(data[0]);
-                //Decide if coin is up or down in the past 24 hours
-                let color;
-                if (data[0].percent_change_24h > 0)
-                    color = 'green';
-                else
-                    color = 'red';
-
-                $("#coinPrice").append('<li class = " ' + color + '">' + parseFloat(data[0].price_usd).toFixed(2) + '</li>');
-            },
-            error: function(data) {
-                console.log("Error connecting to coinmarketcap API");
-            },
-            type: 'GET'
+                    $("#coinPrice").append('<li class = " ' + color + '">' + parseFloat(data[0].price_usd).toFixed(2) + '</li>');
+                },
+                error: function(data) {
+                    console.log("Error connecting to coinmarketcap API");
+                },
+                type: 'GET'
+            });
         });
-    });
+        //Set border color if successful
+        $("#money").addClass('whiteBorder');
+        $("#money").removeClass('yellowBorder');
+    }
+    catch(err){
+        //Set border color if error
+        $("#money").addClass('yellowBorder');
+        $("#money").removeClass('whiteBorder');
+    }
 }
 
 
@@ -178,7 +208,7 @@ function getHUEIP(){
     $.ajax({
         type: 'GET',
         url: "https://www.meethue.com/api/nupnp",
-        success: connectToBridge,
+        success: connectToHueBridge,
         error: function(data) {
             let hueIPAddress = null;
             console.log("Error connecting to Hue API");
@@ -188,55 +218,57 @@ function getHUEIP(){
     
 
 
-function connectToBridge(data){
-        console.log("connectToBridge");
-        //Get user ID from config file
-        let hueUserName = config.HOME.HUE_USER_ID;
+function connectToHueBridge(data){
+    //Get user ID from config file
+    let hueAPIKey = config.HOME.HUE_API_KEY;
 
-        if (hueUserName == null)
-            alert("Missing values from config.js... unable to load HUE module");
-        else{
+    try{
+        let bridgeIP = data[0].internalipaddress;
 
-            let bridgeIP = data[0].internalipaddress;
+        //url for hue bridge api
+        let lightUrl = 'http://' + bridgeIP + '/api/' + hueUserName + '/lights';
 
-            //url for hue bridge api
-            let lightUrl = 'http://' + bridgeIP + '/api/' + hueUserName + '/lights';
+        //Send ajax request
+        $.ajax({
+            type: 'GET',
+            url: lightUrl,
+            success: function(data) {
+                $.each(data, function(index, value){
+                    let brightness = (value.state.bri / 254);
+                    let bulb = '<td class="clearable"><img style = "opacity:'+ brightness + 
+                                '" src = "Images/bulb.png"></img></td>';
 
-            //Send ajax request
-            $.ajax({
-                type: 'GET',
-                url: lightUrl,
-                success: function(data) {
-                    $.each(data, function(index, value){
-                        let brightness = (value.state.bri / 254);
-                        let bulb = '<td class="clearable"><img style = "opacity:'+ brightness + 
-                                    '" src = "Images/bulb.png"></img></td>';
+                    switch(value.name.charAt(0)){
+                        case 'B':
+                            $("#bedroom").append(bulb);
+                            break;
+                        case 'D':
+                            $("#dining").append(bulb);
+                            break;
+                        case 'L':
+                            $("#living").append(bulb);
+                            break;
+                        case 'O':
+                            $("#office").append(bulb);
+                            break;
+                        default:
+                            console.log("Unexpected bulb name: " + value.name);
+                    }
+                });
+            },
+            error: function(data) {
+                throw "Error connecting to Hue API"
+            }
+        });
 
-                        switch(value.name.charAt(0)){
-                            case 'B':
-                                $("#bedroom").append(bulb);
-                                break;
-                            case 'D':
-                                $("#dining").append(bulb);
-                                break;
-                            case 'L':
-                                $("#living").append(bulb);
-                                break;
-                            case 'O':
-                                $("#office").append(bulb);
-                                break;
-                            default:
-                                console.log("Unexpected bulb name: " + value.name);
-                        }
-                    });
-                    
-                },
-                error: function(data) {
-                    console.log("Error connecting to Hue API");
-                }
-            });
-        }
+        //Set border color if successful
+        $("#home").addClass('whiteBorder');
+        $("#home").removeClass('yellowBorder');
     }
-
-
-
+    catch(err){
+        //Set border color if error
+        console.log("Home Error - Setting Caution");
+        $("#home").addClass('yellowBorder');
+        $("#home").removeClass('whiteBorder');
+    } 
+}
